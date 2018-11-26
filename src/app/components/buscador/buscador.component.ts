cf56c6185648aeff641b6d6ca557a8e95b8071aa
fedeatanasoff <fede.atanasoff@gmail.com>
2018-11-26 19:12:04 -0300

    buscador funcionando

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { DepartamentoService } from "../../servicios/departamento.services";
 
 @Component({
-  selector: 'app-buscador',
-  templateUrl: './buscador.component.html',
-  styleUrls: ['./buscador.component.css']
+  selector: "app-buscador",
+  templateUrl: "./buscador.component.html",
+  styleUrls: ["./buscador.component.css"]
 })
 export class BuscadorComponent implements OnInit {
+  atletas: any[] = [];
+  texto: string;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _departamentoService: DepartamentoService
+  ) {}
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.texto = params["texto"];
+      this.atletas = this._departamentoService.buscarAtleta(params["texto"]);
+      console.log(this.atletas);
+      console.log(this.texto);
+    });
   }
-
 }
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html"
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 
   buscarAtleta(texto: string) {
     console.log(texto);
+
+    this.router.navigate(["/buscar", texto]);
   }
 }
